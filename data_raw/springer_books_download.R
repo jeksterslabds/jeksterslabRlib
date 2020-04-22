ce9fdc00a1af89793a22160430ea50acd09b8c89
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-23 01:10:12 +0800

    Added springer_books_download.R

---
+++ data_raw/springer_books_download.R
@@ -0,0 +1,47 @@
+#' ---
+#' title: "Download Springer_Books"
+#' author: "Ivan Jacob Agaloos Pesigan"
+#' date: "`r Sys.Date()`"
+#' output:
+#'   rmarkdown::html_vignette:
+#'     toc: true
+#' ---
+#'
+#' ## Springer Books Directory
+#'
+#+ directory
+dir <- "/media/jeksterslib/books/springer"
+print(dir)
+#'
+#' ## Available Books
+#'
+#' ### PDF
+#'
+#+ pdf
+pdf <- length(
+  list.files(
+    path = dir,
+    pattern = "[[:print:]].pdf"
+  )
+)
+print(pdf)
+#'
+#' ### EPUB
+#'
+#+ epub
+epub <- length(
+  list.files(
+    path = dir,
+    pattern = "[[:print:]].epub"
+  )
+)
+print(epub)
+#'
+#' ## Download Books
+#'
+#+ download
+lib_springer_download(
+  dir = dir,
+  type = "both",
+  par = TRUE
+)
